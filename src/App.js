f293a55924e108380e163b99c2d4254d0684e519
Vishwjeet Singh <vishwjeetsingh594@gmail.com>
2021-07-26 11:55:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import {Map,TileLayer, Marker, Popup } from "react-leaflet";
 
 import Markers from '../src/Components/Marker/Marker';
 
-const places = {
-  NewDelhi : { name: 'New Delhi' , coordinates: [28.613939, 77.209023] , populationdensity: '31,181,376'} 
-}
+
 class App extends Component {
+
+
   render(){
+    const places = {
+      NewDelhi : { name: 'New Delhi' , coordinates: [[28.613939, 77.209023]] , populationdensity: '31,181,376'},
+      Mumbai : { name: 'Mumbai' , coordinates: [19.075983, 72.877655] , populationdensity: '14,974,073'},
+      Chennai : { name: 'Chennai' , coordinates: [13.082680, 80.270721] , populationdensity: '11,235,018'}, 
+      Siddharthnagar : { name: 'SiddharthNagar' , coordinates: [28.613939, 77.209023] , populationdensity: '2,559,297'}
+    }
+    console.log(places.NewDelhi.coordinates);
     return (
       <Map center={[20.593683, 78.962883]}
        zoom={5} 
@@ -27,7 +34,6 @@ class App extends Component {
             New Delhi <br /> <b>Population density is:</b> 31,181,376
           </Popup>
         </Marker>
-
         {/* kolkata display */}
 
         <Marker position={[22.572645, 88.363892]}>
